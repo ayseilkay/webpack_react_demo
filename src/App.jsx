418a3110cf6702e39a3089ed01b134ab31af6261
ayseilkay <aayseilkay@gmail.com>
2022-05-04 22:43:05 +0300

    webpack react demo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HelloWord from './components/HelloWorld'
+function App() {
+  return (
+    <div>
+        <HelloWord/>
+     </div>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HelloWorld() {
+  return (
+    <div>HelloWorld dksldjfsdfj</div>
+  )
+}
+
+export default HelloWorld
